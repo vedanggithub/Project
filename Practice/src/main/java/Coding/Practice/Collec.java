bc219480a1b9be6d51c38478fbdd783baa466611
Vedang <Vedang@whitebox-learning.com>
2019-06-05 17:04:10 -0700

    second commit

--- Practice/src/main/java/Coding/Practice/Collec.java
+++ Practice/src/main/java/Coding/Practice/Collec.java
@@ -3,30 +3,41 @@ package Coding.Practice;
 import java.util.TreeSet;
 
 public class Collec {
-	
-	public static Integer meth(int[] a) {
-		
-		TreeSet<Integer> l = new TreeSet<Integer>();
-		
-		for (int b : a) {
-			
-			l.add(b);
-			
+
+	/*
+	 * public static Integer meth(int[] a) {
+	 * 
+	 * TreeSet<Integer> l = new TreeSet<Integer>();
+	 * 
+	 * for (int b : a) {
+	 * 
+	 * l.add(b);
+	 * 
+	 * } l.pollLast(); //System.out.println("1st Highest number : " + l.pollLast());
+	 * 
+	 * //System.out.println("Second Highest number : " + l.last()); return l.last();
+	 */
+	static int meth(int[] a) {
+		int h1 =0;
+		int h2 = 0;
+		for (int num : a) {
+			if (num > h1) {//2>0
+				h2 = h1;//h2=0
+				h1 = num;//h1=2
+			} else if (num > h2) {
+				h2 = num;
+			}
+
 		}
-		l.pollLast();
-		//System.out.println("1st Highest number : " + l.pollLast());
-		
-		//System.out.println("Second Highest number : " + l.last());
-		return l.last();
+		return h2;
+
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		int[] a = { 2, 5, 6, 9, 8, 6, 7 };
-		
+
 		System.out.println(Collec.meth(a));
-		//System.out.println("1st Highest number : "+Collec.meth(a).pollLast());	
-		//System.out.println("Second Highest number : " +Collec.meth(a).pollLast().);
-		
+
 	}
 }
--- Practice/src/test/java/Coding/Practice/Collectest1.java
+++ Practice/src/test/java/Coding/Practice/Collectest1.java
@@ -11,28 +11,28 @@ import org.testng.annotations.Test;
 
 public class Collectest1{
 	@Test
-	public void test() {
+	public void positive() {
 		Collec help=new Collec();
 		int a[]={1,2,3,4,9};
 		int actual=help.meth(a);
 		Assert.assertEquals(4,actual );
 	}
 	@Test
-	public void test2() {
+	public void nullvalue() {
 		Collec help=new Collec();
-		int a[]={1,-7,8,6};
+		int a[]={0,0,2,0,0,1};
 		int actual=help.meth(a);
 		System.out.println(help.meth(a));
-		Assert.assertEquals(6,actual );
-		Assert.assertTrue(true);
+		Assert.assertEquals(1,actual );
+		//Assert.assertTrue(true);
 	}
 	@Test
-	public void test3() {
+	public void negative() {
 		Collec help=new Collec();
-		int a[]={-1,0,-3,0,-2};
+		int a[]={-1,0,-3,-6,-2};
 		int actual=help.meth(a);
 		//System.out.println(help.meth(a));
 		Assert.assertEquals(-1,actual );
-		Assert.assertTrue(true);
+		//Assert.assertTrue(true);
 }
 }
