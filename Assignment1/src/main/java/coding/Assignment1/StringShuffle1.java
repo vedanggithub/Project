d744b4c19ddae4942ba9d2209c1f27ae4dcf28d0
Vedang <Vedang@whitebox-learning.com>
2019-06-18 14:35:40 -0700

    few changes

--- Assignment1/src/main/java/coding/Assignment1/StringShuffle1.java
+++ Assignment1/src/main/java/coding/Assignment1/StringShuffle1.java
@@ -3,7 +3,7 @@ package coding.Assignment1;
 public class StringShuffle1 {
 
 	public static void main(String[] args) {
-		String str = "ABDEG";
+		String str = "DEAB";
 		String str1 = "AB";
 		String str2 = "DE";
 
@@ -14,36 +14,55 @@ public class StringShuffle1 {
 		} else {
 			System.out.println("it is not valid");
 		}
-
 	}
-
-	public static boolean shuffle(String str, String str1, String str2) {
-		
-		int a = 0, b = 0, c = 0;
-		
-		if (str1.length() + str2.length() != str.length()) {
-			
+	public static boolean shuffle(String str,String str1,String str2) {
+		if (str.length()==0 && str1.length()==0 && str2.length()==0)
+		{
+			return true;
+		}
+		if (str1.length() + str2.length() != str.length()) 
+		{
 			return false;
 		}
-		while (c < str.length()) {
-			
-			if (a < str1.length() && str1.charAt(a) == str.charAt(c)) {
-				
-				a++;
-
-			} else if (b < str2.length() && str2.charAt(b) == str.charAt(c)) {
-				
-				b++;
-			}
-
-			else {
-				
-				return false;
-			}
-			
-			c++;
+		if(str1.length()!=0&& str.charAt(0)==str1.charAt(0))
+		{
+			return shuffle(str.substring(1),str1.substring(1),str2);
+		}
+		if(str2.length()!=0 && str.charAt(0)==str2.charAt(0))
+		{
+			return shuffle(str.substring(1),str1,str2.substring(1));
 		}
 		
-		return true;
+		return false;
 	}
+
+//	public static boolean shuffle(String str, String str1, String str2) {
+//
+//		int a = 0, b = 0, c = 0;
+//
+//		if (str1.length() + str2.length() != str.length()) {
+//
+//			return false;
+//		}
+//		while (c < str.length()) {
+//
+//			if (a < str1.length() && str1.charAt(a) == str.charAt(c)) {
+//
+//				a++;
+//
+//			} else if (b < str2.length() && str2.charAt(b) == str.charAt(c)) {
+//
+//				b++;
+//			}
+//
+//			else {
+//
+//				return false;
+//			}
+//
+//			c++;
+//		}
+//
+//		return true;
+//	}
 }
