cc2b8fa87227e05bd23c5cfe823aa9c355073417
Vedang <Vedang@whitebox-learning.com>
2019-06-19 11:31:33 -0700

    Shuffle with HM

---
+++ Assignment1/src/main/java/coding/Assignment1/ShuffleHp.java
@@ -0,0 +1,49 @@
+package coding.Assignment1;
+
+import java.util.*;
+
+public class ShuffleHp {
+	public static void main(String[] args) {
+		String str = "EADB";// ABDE
+		String str1 = "BA";
+		String str2 = "DE";
+		System.out.println(shuffle(str, str1, str2));
+	}
+
+	public static boolean shuffle(String str, String str1, String str2) {
+		Map<Character, Integer> dp = new HashMap<Character, Integer>();
+		Map<Character, Integer> dp1 = new HashMap<Character, Integer>();
+
+		// int count = 0;
+
+		String temp2 = str1 + str2;
+		if (str != null) {
+			char[] ch = str.toCharArray();
+			for (char c : ch) {
+				if (dp.containsKey(c)) {
+					dp.put(c, dp.get(c) + 1);
+				} else {
+					dp.put(c, 1);
+				}
+			}
+			//System.out.println(dp);
+
+			char[] ch2 = temp2.toCharArray();
+
+			for (char c : ch2) {
+
+				if (dp1.containsKey(c)) {
+					dp1.put(c, dp1.get(c) + 1);
+				} else {
+					dp1.put(c, 1);
+				}
+			}
+			//System.out.println(dp1);
+			if (dp.equals(dp1)) {
+				return true;
+			}
+		}
+		return false;
+
+	}
+}
--- Assignment1/src/test/java/coding/Assignment1/ShuffleHpTest.java
+++ Assignment1/src/test/java/coding/Assignment1/ShuffleHpTest.java
@@ -4,16 +4,13 @@ import static org.testng.Assert.assertEquals;
 
 import org.testng.annotations.Test;
 
-//import junit.framework.TestCase;
-
-public class StringShuffle1Test {
-
+public class ShuffleHpTest {
 	@Test
 	public void validString() {
 		String str1="WX";
 		String str2="YZ";
-		String str="WXYZ";
-		boolean actual = StringShuffle1.shuffle(str, str1, str2);
+		String str="ZYWX";
+		boolean actual = ShuffleHp.shuffle(str, str1, str2);
 		assertEquals(actual, true);
 	}
 	
@@ -21,8 +18,8 @@ public class StringShuffle1Test {
 	public void inValidString() {
 		String str1="PQR";
 		String str2="MNO";
-		String str="LMNOPQR";
-		boolean actual = StringShuffle1.shuffle(str, str1, str2);
+		String str="NMLOPQR";
+		boolean actual = ShuffleHp.shuffle(str, str1, str2);
 		assertEquals(actual, false);
 	}
 	@Test
@@ -30,15 +27,15 @@ public class StringShuffle1Test {
 		String str1="123";
 		String str2="45";
 		String str="12345";
-		boolean actual = StringShuffle1.shuffle(str, str1, str2);
+		boolean actual = ShuffleHp.shuffle(str, str1, str2);
 		assertEquals(actual, true);
 	}
 	@Test
 	public void negIntString() {
 		String str1="-1-2-3";
 		String str2="-4-5";
-		String str="-1-2-3-4-5";
-		boolean actual = StringShuffle1.shuffle(str, str1, str2);
+		String str="-1-3-2-4-5";
+		boolean actual = ShuffleHp.shuffle(str, str1, str2);
 		assertEquals(actual, true);
 	}
 	@Test
@@ -46,7 +43,8 @@ public class StringShuffle1Test {
 		String str1=" ";
 		String str2=" ";
 		String str="  ";
-		boolean actual = StringShuffle1.shuffle(str, str1, str2);
+		boolean actual = ShuffleHp.shuffle(str, str1, str2);
 		assertEquals(actual, true);
 	}
+
 }
